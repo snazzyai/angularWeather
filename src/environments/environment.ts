4b5092ad1809f025d6df1663d0b7d3b32d14eb1e
abdulsalamibrahim <abdulsalamibrahim7@gmail.com>
2022-03-10 02:59:23 +0100

    created environment data

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '39ee17d660bd7358d6f112dc3e334ff9',
+  apiUrl: 'https://api.openweathermap.org/data/2.5/'
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
